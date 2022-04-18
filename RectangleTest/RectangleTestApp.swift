c8dee528aca023abd2c89037e1fc88a13dce55c5
John <johnathanlee72@hotmail.com>
2022-04-18 16:04:15 -0700

    Initial Commit

---
+++ RectangleTest/RectangleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RectangleTestApp.swift
+//  RectangleTest
+//
+//  Created by Johnathan Lee on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct RectangleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
